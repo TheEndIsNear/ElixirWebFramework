32eec32aa1c371fe024b74b7921c27cf777e9668
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2023-08-04 20:29:19 -0600

    add tests around the test server

--- cowboy_example/lib/cowboy_example/application.ex
+++ cowboy_example/lib/cowboy_example/application.ex
@@ -8,7 +8,7 @@ defmodule CowboyExample.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Task, fn -> CowboyExample.Server.start(4040) end}
+      {Task, fn -> CowboyExample.Server.start(port()) end}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -16,4 +16,6 @@ defmodule CowboyExample.Application do
     opts = [strategy: :one_for_one, name: CowboyExample.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp port, do: Application.get_env(:cowboy_example, :port, 4040)
 end
--- cowboy_example/test/cowboy_example_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CowboyExampleTest do
-  use ExUnit.Case
-  doctest CowboyExample
-
-  test "greets the world" do
-    assert CowboyExample.hello() == :world
-  end
-end
